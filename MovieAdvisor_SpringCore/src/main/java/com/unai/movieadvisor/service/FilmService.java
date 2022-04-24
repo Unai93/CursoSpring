11f36f3ff2e0fd2da684ce6660189306b95958a2
Unai93 <85676865+Unai93@users.noreply.github.com>
2022-04-24 18:14:14 +0200

    Add files via upload

---
+++ MovieAdvisor_SpringCore/src/main/java/com/unai/movieadvisor/service/FilmService.java
@@ -0,0 +1,61 @@
+package com.unai.movieadvisor.service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.unai.movieadvisor.dao.FilmDao;
+import com.unai.movieadvisor.model.Film;
+
+@Service
+public class FilmService {
+	@Autowired
+	FilmDao filmDao;
+
+	@Autowired
+	FilmQueryService queryService;
+
+	public Collection<String> findAllGenres() {
+		List<String> result = null;
+		// @formatter:off
+		result = filmDao.findAll()
+				.stream()
+				.map(f -> f.getGenres())
+				.flatMap(lista -> lista.stream())
+				.distinct()
+				.sorted()
+				.collect(Collectors.toList());
+
+		// @formatter:on
+		return result;
+	}
+
+	public Collection<Film> findByAnyGenre(String... genres) {
+
+		return queryService.anyGenre(genres).exec();
+
+	}
+
+	public Collection<Film> findByAllGenres(String... genres) {
+		return queryService.allGenres(genres).exec();
+	}
+
+	public Collection<Film> findByYear(String year) {
+		return queryService.year(year).exec();
+	}
+
+	public Collection<Film> findBetweenYears(String from, String to) {
+		return queryService.betweenYears(from, to).exec();
+	}
+
+	public Collection<Film> findByTitleContains(String title) {
+		return queryService.titleContains(title).exec();
+	}
+
+	public Collection<Film> findAll() {
+		return filmDao.findAll();
+	}
+}
